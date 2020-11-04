bc5ad5195890df74da731bf0fc2f5ee3b26908a6
Karen Xu <xyira@amazon.com>
2020-11-04 15:43:08 -0500

    Update logger.cc log method's TODO and inject timestamp

--- sdk/src/logs/logger.cc
+++ sdk/src/logs/logger.cc
@@ -39,18 +39,19 @@ void Logger::log(const opentelemetry::logs::LogRecord &record) noexcept
   if (!IsEnabled(record.severity))
     return;
 
-  // Inject timestamp
-  // record.timestamp = core::SystemTimestamp(std::chrono::system_clock::now());
-  // TODO: Remove const from record to allow for value injection
-
-  // Send the log record to our processor
-  // Convert the LogRecord to the heap
-  /*
-   TODO: Make the API call log(*LogRecord) such that the SDK doesn't
-   need to convert to the heap
+  /** 
+   * Convert the LogRecord to the heap first before sending to processor. 
+   * TODO: Change the API log(LogRecord) function to log(*LogRecord) so the following line converting to a heap can be removed
   */
   auto record_pointer =
       std::unique_ptr<opentelemetry::logs::LogRecord>(new opentelemetry::logs::LogRecord(record));
+  
+  // Inject timestamp
+  record_pointer->timestamp = core::SystemTimestamp(std::chrono::system_clock::now());
+  
+  // TODO: inject traceid/spanid later
+
+  // Send the log record to the processor
   GetProcessor()->OnReceive(std::move(record_pointer));
 }
 
