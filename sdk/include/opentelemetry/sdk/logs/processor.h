98d3d6d8660e99cd1c2dfb5de1df17d9830ceeef
Karen Xu <xyira@amazon.com>
2020-11-24 16:42:52 -0500

    review comments

--- sdk/include/opentelemetry/sdk/logs/processor.h
+++ sdk/include/opentelemetry/sdk/logs/processor.h
@@ -25,12 +25,6 @@ namespace sdk
 {
 namespace logs
 {
-enum class ShutdownResult
-{
-  kSuccess = 0,
-  kFailure = 1,
-  kTimeout = 2
-};
 /**
  * The Log Processor is responsible for passing log records
  * to the configured exporter.
