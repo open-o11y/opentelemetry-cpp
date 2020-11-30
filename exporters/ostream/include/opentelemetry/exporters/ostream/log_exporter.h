80c75d5591060148a8ec59ac65a6cd6a634e5c7d
Karen Xu <xyira@amazon.com>
2020-11-30 07:17:46 -0500

    Add Ostream log exporter and tests

---
+++ exporters/ostream/include/opentelemetry/exporters/ostream/log_exporter.h
@@ -0,0 +1,145 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "opentelemetry/nostd/type_traits.h"
+#include "opentelemetry/sdk/logs/exporter.h"
+#include "opentelemetry/sdk/logs/log_record.h"
+#include "opentelemetry/version.h"
+
+#include <iostream>
+#include <sstream>
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace exporter
+{
+namespace logs
+{
+/**
+ * The OStreamLogExporter exports logs through an ostream (default set to std::cout)
+ */
+class OStreamLogExporter final : public opentelemetry::sdk::logs::LogExporter
+{
+public:
+  /**
+   * Create an OStreamLogExporter. This constructor takes in a reference to an ostream that the
+   * Export() method will send log data into. The default ostream is set to stdout.
+   */
+  explicit OStreamLogExporter(std::ostream &sout = std::cout) noexcept;
+
+  /*********************************** Helper functions ************************/
+
+
+  /*
+    print_value is used to print out the value of an attribute within a vector.
+    These values are held in a variant which makes the process of printing them much more
+    complicated.
+  */
+
+  template <typename T>
+  void print_value(const T &item)
+  {
+    sout_ << item;
+  }
+
+  template <typename T>
+  void print_value(const std::vector<T> &vec)
+  {
+    sout_ << '[';
+    size_t i  = 1;
+    size_t sz = vec.size();
+    for (auto v : vec)
+    {
+      sout_ << v;
+      if (i != sz)
+        sout_ << ',' << ' ';
+      i++;
+    };
+    sout_ << ']';
+  }
+
+// Prior to C++14, generic lambda is not available so fallback to functor.
+#if __cplusplus < 201402L
+
+  class OwnedAttributeValueVisitor
+  {
+  public:
+    OwnedAttributeValueVisitor(OStreamLogExporter &exporter) : exporter_(exporter) {}
+
+    template <typename T>
+    void operator()(T &&arg)
+    {
+      exporter_.print_value(arg);
+    }
+
+  private:
+    OStreamLogExporter &exporter_;
+  };
+
+#endif
+
+  void print_value(sdk::common::OwnedAttributeValue &value)
+  {
+#if __cplusplus < 201402L
+    nostd::visit(OwnedAttributeValueVisitor(*this), value);
+#else
+    nostd::visit([this](auto &&arg) { print_value(arg); }, value);
+#endif
+  }
+
+  void printMap(std::unordered_map<std::string, sdk::common::OwnedAttributeValue> map)
+  {
+    size_t size = map.size();
+    size_t i    = 1;
+    for (auto kv : map)
+    {
+      sout_ << "{" << kv.first << ": ";
+      print_value(kv.second);
+      sout_ << "}"; 
+
+      if (i != size)
+        sout_ << ", ";
+      i++;
+    }
+  }
+
+  /***********************  LogExporter overloaded methods ***********************/
+
+  std::unique_ptr<sdk::logs::Recordable> MakeRecordable() noexcept override;
+
+  /**
+   * Exports a span of logs sent from the processor.
+   */
+  opentelemetry::sdk::logs::ExportResult Export(
+      const opentelemetry::nostd::span<std::unique_ptr<sdk::logs::Recordable>>
+          &records) noexcept override;
+
+  /**
+   * Marks the OStream Log Exporter as shut down.
+   */
+  bool Shutdown(  
+      std::chrono::microseconds timeout = std::chrono::microseconds::max()) noexcept override;
+
+private:
+  // The OStream to send the logs to
+  std::ostream &sout_;
+  // Whether this exporter has been shut down
+  bool is_shutdown_ = false;
+};
+}  // namespace logs
+}  // namespace exporter
+OPENTELEMETRY_END_NAMESPACE
---
+++ exporters/ostream/src/log_exporter.cc
@@ -0,0 +1,102 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "opentelemetry/exporters/ostream/log_exporter.h"
+
+#include <iostream>
+
+namespace nostd   = opentelemetry::nostd;
+namespace sdklogs = opentelemetry::sdk::logs;
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace exporter
+{
+namespace logs
+{
+OStreamLogExporter::OStreamLogExporter(std::ostream &sout) noexcept : sout_(sout) {}
+
+std::unique_ptr<sdklogs::Recordable> OStreamLogExporter::MakeRecordable() noexcept
+{
+  return std::unique_ptr<sdklogs::Recordable>(new sdklogs::LogRecord());
+}
+
+sdklogs::ExportResult OStreamLogExporter::Export(
+    const nostd::span<std::unique_ptr<sdklogs::Recordable>> &records) noexcept
+{
+  if (is_shutdown_)
+  {
+    return sdklogs::ExportResult::kFailure;
+  }
+
+  for (auto &record : records)
+  {
+    // Convert recordable to a LogRecord so that the getters of the LogRecord can be used 
+     auto log_record = std::unique_ptr<sdklogs::LogRecord>(
+        static_cast<sdklogs::LogRecord *>(record.release()));
+
+    if(log_record == nullptr){
+      // Error "recordable data was lost"
+      continue; 
+    }
+    
+    // Convert trace, spanid, traceflags into exportable representation
+    constexpr int trace_id_bytes = 16;
+    constexpr int span_id_bytes = 8;
+    constexpr int trace_flags_bytes = 1;
+
+    char trace_id[trace_id_bytes * 2] = {0};
+    char span_id[span_id_bytes * 2] = {0};
+    char trace_flags[trace_flags_bytes * 2] = {0};
+
+    log_record->GetTraceId().ToLowerBase16(trace_id);
+    log_record->GetSpanId().ToLowerBase16(span_id);
+    log_record->GetTraceFlags().ToLowerBase16(trace_flags);
+
+    // Print out each field of the log record
+    sout_ << "{\n"
+          << "  timestamp     : " << log_record->GetTimestamp().time_since_epoch().count() << "\n"
+          << "  severity_num  : " << static_cast<int>(log_record->GetSeverity()) << "\n"
+          << "  severity_text : " << opentelemetry::logs::SeverityNumToText[static_cast<int>(log_record->GetSeverity())] << "\n"
+          << "  name          : " << log_record->GetName() << "\n"
+          << "  body          : " << log_record->GetBody() << "\n"
+          << "  resource      : {";
+
+    printMap(log_record->GetResource());
+
+    sout_ << "}\n"
+          << "  attributes    : {";
+
+    printMap(log_record->GetAttributes()); 
+
+    sout_ << "}\n"
+          << "  trace_id      : " << std::string(trace_id, 32) << "\n"
+          << "  span_id       : " << std::string(span_id, 16) << "\n"
+          << "  trace_flags   : " << std::string(trace_flags, 2) << "\n"
+          << "}\n";
+  }
+
+  return sdklogs::ExportResult::kSuccess;
+}
+
+bool OStreamLogExporter::Shutdown(std::chrono::microseconds timeout) noexcept
+{
+  is_shutdown_ = true;
+  return true;
+}
+
+}  // namespace logs
+}  // namespace exporter
+OPENTELEMETRY_END_NAMESPACE
