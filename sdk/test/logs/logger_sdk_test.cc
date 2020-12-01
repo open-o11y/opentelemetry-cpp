4c50ef2438178b7ee42b0bfdc87a681aba35a2c4
Seufert <markseufert1@gmail.com>
2020-12-01 14:02:51 -0700

    Simplified logger context injection, added unit tests

--- sdk/test/logs/logger_sdk_test.cc
+++ sdk/test/logs/logger_sdk_test.cc
@@ -15,10 +15,15 @@
  */
 
 #include "opentelemetry/sdk/logs/logger.h"
+#include "opentelemetry/sdk/trace/tracer_provider.h"
+#include "opentelemetry/trace/provider.h"
+#include "opentelemetry/exporters/memory/in_memory_span_exporter.h"
+#include "opentelemetry/sdk/trace/simple_processor.h"
 
 #include <gtest/gtest.h>
 
 using namespace opentelemetry::sdk::logs;
+namespace sdktrace  = opentelemetry::sdk::trace;
 
 TEST(LoggerSDK, LoggerName)
 {
@@ -68,9 +73,35 @@ TEST(LoggerSDK, DefaultValueInjection)
   logger->Log(r);
 
   // Check that the log record has injected values
-
   // Timestamp shouldn't equal 0
   ASSERT_NE(r->timestamp, opentelemetry::core::SystemTimestamp(std::chrono::seconds(0)));
 
-  // TODO: Add checks for traceid, spanid, and traceflags once it gets added
+  //Check that the traceid, spanid, and traceflags are not valid since there is no trace context
+  ASSERT_FALSE(r->trace_id.IsValid());
+  ASSERT_FALSE(r->span_id.IsValid());
+  ASSERT_FALSE(r->trace_flags.IsSampled());
+
+  // To test traceid/spanid/traceflags injection, initialize the tracing pipeline
+  std::unique_ptr<opentelemetry::exporter::memory::InMemorySpanExporter> trace_exporter(new opentelemetry::exporter::memory::InMemorySpanExporter());
+  auto trace_processor = std::make_shared<opentelemetry::sdk::trace::SimpleSpanProcessor>(std::move(trace_exporter)); 
+  auto trace_provider = opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
+      new sdktrace::TracerProvider(trace_processor));
+  opentelemetry::trace::Provider::SetTracerProvider(trace_provider);
+
+  //Create a tracer and start a span for span context
+  auto tracer = trace_provider->GetTracer("foo_library");
+  auto span_first  = tracer->StartSpan("span 1");
+  auto scope_first = tracer->WithActiveSpan(span_first);
+  auto span_second = tracer->StartSpan("span 2");
+
+  // Log a sample log record to the processor
+  auto r2  = std::shared_ptr<opentelemetry::logs::LogRecord>(new opentelemetry::logs::LogRecord);
+  r2->name = "Test log";
+  logger->Log(r2);
+  
+  //Check that the traceid, spanid, and traceflags were injected from the span context properly
+  auto span_context = tracer->GetCurrentSpan()->GetContext();
+  ASSERT_EQ(r2->trace_id, span_context.trace_id());
+  ASSERT_EQ(r2->span_id, span_context.span_id());
+  ASSERT_EQ(r2->trace_flags, span_context.trace_flags());
 }
