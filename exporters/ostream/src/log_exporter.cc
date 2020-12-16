449b00ff25c2a8bb130591ecd8f6936b9fa69626
Karen Xu <xyira@amazon.com>
2020-12-16 15:03:20 -0500

    add s

--- exporters/ostream/src/log_exporter.cc
+++ exporters/ostream/src/log_exporter.cc
@@ -82,7 +82,7 @@ void print_value(sdk::common::OwnedAttributeValue &value, std::ostream &sout)
 #if __cplusplus < 201402L
   nostd::visit(OwnedAttributeValueVisitor(sout), value);
 #else
-  nostd::visit([](auto &&arg) { print_value(arg, sout); }, value);
+  nostd::visit([&sout](auto &&arg) { print_value(arg, sout); }, value);
 #endif
 }
 
