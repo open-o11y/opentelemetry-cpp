6e809187d9b9dc2abc71c8631af0cacd46f6a19a
Karen Xu <xyira@amazon.com>
2020-12-30 19:47:40 -0500

    Address comments

--- sdk/include/opentelemetry/sdk/common/attribute_utils.h
+++ sdk/include/opentelemetry/sdk/common/attribute_utils.h
@@ -38,9 +38,6 @@ using OwnedAttributeValue = nostd::variant<bool,
                                            uint64_t,
                                            double,
                                            std::string,
-#ifdef HAVE_CSTRING_TYPE
-                                           const char *,
-#endif
 #ifdef HAVE_SPAN_BYTE
                                            std::vector<uint8_t>,
 #endif
@@ -67,7 +64,6 @@ struct AttributeConverter
   {
     return OwnedAttributeValue(std::string(v.data(), v.size()));
   }
-  OwnedAttributeValue operator()(const char *s) { return OwnedAttributeValue(std::string(s)); }
 #ifdef HAVE_SPAN_BYTE
   OwnedAttributeValue operator()(nostd::span<const uint8_t> v) { return convertSpan<uint8_t>(v); }
 #endif
